0babc0f86a0f291b4f2910b9c30665f76659ae18
João Gabriel Saliba <joao_salibaa@hotmail.com>
2023-05-19 17:39:33 -0300

    creating and using the first component

---
+++ crud/src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css'],
+})
+export class FirstComponentComponent {
+  constructor() {}
+  ngOnIniti(): void {}
+}
