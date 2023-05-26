14e2d9386b935ea155bb6526e8836ce321e9f0e3
João Gabriel Saliba <joao_salibaa@hotmail.com>
2023-05-25 22:44:09 -0300

    using inteface (typeScript)

--- crud/src/app/components/list-render/list-render.component.ts
+++ crud/src/app/components/list-render/list-render.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Animal } from 'src/app/interfaces/Animal';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent {
-  animals = [
-    { name: 'mel', type: 'dog' },
-    { name: 'meg', type: 'dog' },
-    { name: 'tupa', type: 'cat' },
-    { name: 'Pirulito', type: 'House' },
+  animals: Animal[] = [
+    { name: 'mel', type: 'dog', age: 2 },
+    { name: 'meg', type: 'dog', age: 10 },
+    { name: 'tupa', type: 'cat', age: 1 },
+    { name: 'Pirulito', type: 'House', age: 15 },
   ];
-  animal = {
+  animal: Animal = {
     name: 'TUTU',
     type: 'FISH',
+    age: 0.5,
   };
+  animalDetails = '';
+
+  showAge(animal: Animal) {
+    this.animalDetails = `The pet ${animal.name} age is ${animal.age} `;
+    // console.log(this.animalDetails);
+  }
 }
